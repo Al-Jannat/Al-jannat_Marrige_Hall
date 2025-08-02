523dae298af99322cb94cfe861d59e0b42f44e53
soul <soul@soul.soul>
2025-08-02 22:47:00 +0500

    15 commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = (process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000').replace(/\/+$/, '');
+const API_URL = (process.env.REACT_APP_API_BASE_URL ||'https://al-jannat-marrige-hall-czgy.vercel.app');
 
 export const getServices = async () => {
   try {
